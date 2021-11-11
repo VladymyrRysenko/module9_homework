39e834ebfb3deee8711129e7fb42ee6be4f09e97
VladymyrRysenko <nva@centre.kharkov.ua>
2021-11-11 23:40:20 +0200

    Add files via upload

---
+++ 9.3/script.js
@@ -0,0 +1,61 @@
+function useRequest(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  
+  xhr.onload = function() {
+    if (xhr.status != 200) {
+      console.log('Статус ответа: ', xhr.status);
+    } else {
+      const result = JSON.parse(xhr.response);
+      if (callback) {
+        callback(result);
+      }
+    }
+  };
+  
+  xhr.onerror = function() {
+    console.log('Ошибка! Статус ответа: ', xhr.status);
+  };
+  
+  xhr.send();
+};
+
+// Ищем ноду для вставки результата запроса
+const resultNode = document.querySelector('.j-result');
+// Ищем кнопку, по нажатии на которую будет запрос
+const btnNode = document.querySelector('.j-btn-request');
+
+/**
+  * Функция обработки полученного результата
+  * apiData - объект с результатом запроса
+  */
+function displayResult(apiData) {
+  let cards = '';
+  // console.log('start cards', cards);
+  
+  apiData.forEach(item => {
+    const cardBlock = `
+      <div class="card">
+        <img
+          src="${item.download_url}"
+          class="card-image"
+        />
+        <p>${item.author}</p>
+      </div>
+    `;
+    cards = cards + cardBlock;
+  });
+      
+  resultNode.innerHTML = cards;
+}
+
+// Вешаем обработчик на кнопку для запроса
+btnNode.addEventListener('click', () => {
+  let value;
+  value = document.querySelector('input').value  
+
+ if (value >= 1 && value <= 10){
+  useRequest(`https://picsum.photos/v2/list?limit=${value}`, displayResult );
+ } else {
+ alert("число вне диапазона от 1 до 10") };
+ });
