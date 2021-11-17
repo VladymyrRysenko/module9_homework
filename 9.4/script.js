dffd517cb1b76a934919a511ea2a22706d49cfa3
VladymyrRysenko <nva@centre.kharkov.ua>
2021-11-17 23:50:38 +0200

    Update script.js

--- 9.4/script.js
+++ 9.4/script.js
@@ -7,28 +7,24 @@ btnNode.addEventListener('click', () => {
   const inputW =  document.querySelector('.input2').value;
   if(inputH < 100 || inputH > 300 || inputW < 100 || inputW > 300) {
     resultNode.innerHTML = 'одно из чисел вне диапазона от 100 до 300';
-  } esle {
-    fetch(`https://picsum.photos/${input1.value}/${input2.value},`)
-    .then((response) => {
-      
-  }
-}) 
-
-function displayResult(apiData) {
-  let cards = '';
-  
-  apiData.forEach(item => {
-    const cardBlock = `
-      <div class="card">
-        <img
-          src="${item.download_url}"
+  } else {
+       fetch(`https://picsum.photos/${inputH}/${inputW}`)
+      .then((response) => {
+          let cards = '';
+          const cardBlock = `
+          <div class="card">
+          <img src="${response.url}"
           class="card-image"
-        />
-        <p>${item.author}</p>
-      </div>
-    `;
-    cards = cards + cardBlock;
-  });
-      
-  resultNode.innerHTML = cards;
-}
+          />
+          </div>
+       `;
+       cards = cards + cardBlock;
+       resultNode.innerHTML = cards;
+  })
+    .catch(function() { console.log('error') }); 
+  }
+  })
+
+
+
+ 
